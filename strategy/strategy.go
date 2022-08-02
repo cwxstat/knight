af6c1aa37b639f85435bc142b0e62ca6829f2c4d
Mike Chirico (https://mchirico.github.io/) <mchirico@gmail.com>
2022-08-02 16:34:58 -0400

    update-automated-save

--- strategy/strategy.go
+++ strategy/strategy.go
@@ -5,6 +5,16 @@ import (
 	"github.com/cwxstat/knight/knight"
 )
 
+type Record struct {
+	options [][]int
+	chosen  [][]int
+}
+
+func (r *Record) Add(options, chosen []int) {
+	r.options = append(r.options, options)
+	r.chosen = append(r.chosen, chosen)
+}
+
 func Options(b *board.Board) []int {
 	valid := []int{}
 	k := knight.NewKnight()
